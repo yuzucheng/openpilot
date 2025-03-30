ff18a646f7cb807fbee1194445fed293e3f090d3
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-03-30 21:16:31 +0800

    添加Host和MAPBOX_TOKEN

--- launch_env.sh
+++ launch_env.sh
@@ -6,6 +6,10 @@ export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
+export ATHENA_HOST='ws://op.caminfo.cn:7776'
+export API_HOST='http://op.caminfo.cn:7777'
+export MAPBOX_TOKEN='pk.eyJ1IjoibWF3ZWl5dXdlaXdlaSIsImEiOiJjbGtrdjhhMHUwbWwzM3VwYzVtbXkwY2lrIn0.-u-QP8-tfrhAIpvQiNMsOw'
+
 if [ -z "$AGNOS_VERSION" ]; then
   export AGNOS_VERSION="10.1"
 fi
