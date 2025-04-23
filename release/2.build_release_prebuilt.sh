f47891345991d0c98ec05aefae3d63888b586824
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-04-23 18:33:27 +0800

    删除文件命令增加||true

--- release/2.build_release_prebuilt.sh
+++ release/2.build_release_prebuilt.sh
@@ -14,9 +14,9 @@ find . -name 'moc_*' -delete
 find . -name '*.cc' -delete
 find . -name '__pycache__' -delete
 find selfdrive/ui/ -name '*.h' -delete
-rm -rf .sconsign.dblite Jenkinsfile release/
-rm selfdrive/modeld/models/supercombo.onnx
-rm -rf selfdrive/ui/replay/
+rm -rf .sconsign.dblite Jenkinsfile release/ || true
+rm selfdrive/modeld/models/supercombo.onnx || true
+rm -rf selfdrive/ui/replay/ || true
 
 find third_party/ -name '*x86*' -exec rm -r {} +
 find third_party/ -name '*Darwin*' -exec rm -r {} +
